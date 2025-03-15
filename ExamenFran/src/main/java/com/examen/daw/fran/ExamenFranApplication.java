2abe5d52ba8643cb7a16b9c55fe1f5b3a8158fbb
FranYuan <fran.yuan.0804@gmail.com>
2025-03-15 10:46:22 +0100

    subir en feature

---
+++ ExamenFran/src/main/java/com/examen/daw/fran/ExamenFranApplication.java
@@ -0,0 +1,13 @@
+package com.examen.daw.fran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFranApplication.class, args);
+	}
+
+}
---
+++ ExamenFran/src/test/java/com/examen/daw/fran/ExamenFranApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.daw.fran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
